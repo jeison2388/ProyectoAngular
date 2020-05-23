ccc4410e2081cb8aed56b2757413bc4901be5e48
zjimenez110492 <zjimenez@unicauca.edu.co>
2020-05-22 22:36:51 -0500

    equipos quemados

--- src/app/views/competition/admin-competition/tab-competition/teams/list-teams/list-teams.component.ts
+++ src/app/views/competition/admin-competition/tab-competition/teams/list-teams/list-teams.component.ts
@@ -8,9 +8,29 @@ export class ListTeamsComponent implements OnInit {
 
   /****************************************VARIABLES DE SALIDA DEL COMPONENTE*********************** */
   @Output() showAddTeam = new EventEmitter<{showAddTeam: boolean}>();
-
+  equipos: [
+    {
+      numero: 1,
+      Logo: "u496",
+      NombreEquipo:"HOTEL ACHALAY",
+      Delegado:"ANDRES FELIPE RESTREPO",
+      identificacion:1232353453,
+      telefono:23453456567,
+      estado:"Pendiente de pago"
+    },
+    {
+      numero: 1,
+      Logo: "u497",
+      NombreEquipo:"DIAGNOSTIMOTOS",
+      Delegado:"IGNACIO VIVEROS",
+      identificacion:1232353453,
+      telefono:23453456567,
+      estado:"Pendiente realizado"
+    }
+  ];
   constructor() { }
 
+  
   ngOnInit() {
   }
 
